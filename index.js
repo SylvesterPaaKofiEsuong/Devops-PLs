b6d0f468c31018ec49560692266d5531b42e4627
Sylvester <sygycaiat@gmail.com>
2025-10-03 21:34:54 +0000

    Feat: Add CI Pipeline

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("DevOps");
+console.log("Hello World");
+console.log("I'm learning CI/CD Usinng GitHhub Actions");
---
+++ test.js
@@ -0,0 +1,4 @@
+console.log('Starting tests....');
+setTimeout(() => console.log('Waiting for 3 seconds....'),);
+console.log('Test Complete');
+
